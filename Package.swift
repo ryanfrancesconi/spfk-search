4f9c3b1b6d4b85cfd6535521168a65c56d194e6e
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-02-20 11:17:12 -0800

    reduce ios version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "spfk-search",
-    platforms: [.macOS(.v14), .iOS(.v26),],
+    platforms: [.macOS(.v14), .iOS(.v17),],
     products: [
         .library(
             name: "SPFKSearch",
