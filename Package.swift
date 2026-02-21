2d2e63c5043a5e80eff1f6a1d063f2f530142352
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-02-21 08:59:19 -0800

    package versions

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "0.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "0.0.3"),
         .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.1"),
         .package(url: "https://github.com/ryanfrancesconi/FuzzyMatch.git", branch: "development"),
 
