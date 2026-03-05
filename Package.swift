d3ea874cdf8a6f7db3e586c9b7bc14473e3b5570
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-03-05 07:43:54 -0800

    up package requirements to current minimums

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "0.0.3"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.5"),
         .package(url: "https://github.com/ordo-one/FuzzyMatch.git", from: "1.2.0"),
 
     ],
