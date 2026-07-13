40c23615174f50fa4fea7a84c319cd128779afa6
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-07-12 17:55:33 -0700

    Update dependency version minimums to current tags

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "1.0.1"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "1.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "1.2.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "1.1.0"),
         .package(url: "https://github.com/ordo-one/FuzzyMatch.git", from: "1.4.0"),
 
     ],
